b58092b9a3a7d0166dddc98b7e88a625e3195294
Lior Shtivelman <lior.shtivelman@gmail.com>
2022-04-30 12:19:44 +0300

    Remove console logs

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ const puppeteer = require('puppeteer');
 
 
 (async () => {
-    let browser;
     browser = await puppeteer.launch({ headless: true, executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome' });
     const page = await browser.newPage();
     await page.goto('https://ynet.co.il');
     await page.waitForSelector('.WeatherViewInHeader');
     const weatherDiv = await page.$('.WeatherViewInHeader');
-    console.log('weatherDiv', weatherDiv)
 
     const anchorLinkSelector = '.weathertempsdiv a span.weathertemps';
     const tempAnchorLink = await weatherDiv.$(anchorLinkSelector);
