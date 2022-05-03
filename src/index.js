1561051f07a1cfc4e8308d373407ede178c0a276
Lior Shtivelman <lior.shtivelman@gmail.com>
2022-05-03 22:38:35 +0300

    Minor fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const puppeteer = require('puppeteer');
 
-
 (async () => {
     browser = await puppeteer.launch({ headless: true, executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome' });
     const page = await browser.newPage();
